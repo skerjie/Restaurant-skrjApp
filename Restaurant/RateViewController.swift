8e88ef5211a8b4237b48006092a75dd459dfe699
skerjie <skerjie@live.com>
2016-10-25 18:29:09 +0300

    добавил новый view, добавил кнопки, добавил эффект размытия

---
+++ Restaurant/RateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RateViewController.swift
+//  Eateries
+//
+//  Created by Andrei Palonski on 25.10.16.
+//  Copyright © 2016 Andrei Palonski. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
