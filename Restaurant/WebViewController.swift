3b2062bfbf8091a98b46a7c1d390775ed4e27466
skerjie <skerjie@live.com>
2016-11-04 16:43:26 +0300

    более менее рабочий проект, единственное, что нужно доделать, так это к кор дате прикрутить рейтинг

---
+++ Restaurant/WebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewController.swift
+//  Eateries
+//
+//  Created by Andrei Palonski on 02.11.16.
+//  Copyright © 2016 Andrei Palonski. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
